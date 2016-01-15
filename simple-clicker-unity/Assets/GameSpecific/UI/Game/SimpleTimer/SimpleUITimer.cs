5a1bc31ec84a76f03d0d32bd6b78182d7fea7502
Darren Tsung <darren.tsung@gmail.com>
2016-01-15 06:12:16 -0800

    Fix UI scaling issues somewhat - good enough!

--- simple-clicker-unity/Assets/GameSpecific/UI/Game/SimpleTimer/SimpleUITimer.cs
+++ simple-clicker-unity/Assets/GameSpecific/UI/Game/SimpleTimer/SimpleUITimer.cs
@@ -9,39 +9,32 @@ namespace DT.Game {
 		public void SetTimeRemainingProvider(ITimeRemainingProvider timeRemainingProvider) {
 			this._timeRemainingProvider = timeRemainingProvider;
 		}
-		
-		
+
+
 		// PRAGMA MARK - Internal
 		[SerializeField]
 		private Color _maxColor;
 		[SerializeField]
 		private Color _minColor;
-		
-		[SerializeField] 
+
+		[SerializeField]
 		private Image _barImage;
-		[SerializeField, ReadOnly]
-		private float _baseWidth;
-		
+
 		private ITimeRemainingProvider _timeRemainingProvider;
-		
-		
-		protected void Awake() {
-			RectTransform rectTransform = (RectTransform)transform;
-			this._baseWidth = rectTransform.sizeDelta.x;
-		}
-		
+
+
 		protected void Update() {
 			this.UpdateDisplay();
 		}
-		
+
 		protected void UpdateDisplay() {
 			float percentTimeRemaining = _timeRemainingProvider.PercentTimeRemaining;
-			
+
 			Color displayColor = Color.Lerp(_minColor, _maxColor, percentTimeRemaining);
 			_barImage.color = displayColor;
-			
+
 			RectTransform rectTransform = (RectTransform)transform;
-			rectTransform.sizeDelta = rectTransform.sizeDelta.SetX(this._baseWidth * percentTimeRemaining);
+			rectTransform.anchorMax = rectTransform.anchorMax.SetX(percentTimeRemaining);
 		}
 	}
 }
--- simple-clicker-unity/Assets/GameSpecific/UI/TitleScreen/TitleScreenView.cs
+++ simple-clicker-unity/Assets/GameSpecific/UI/TitleScreen/TitleScreenView.cs
@@ -11,41 +11,42 @@ namespace DT.Game {
 			this.StartCoroutine(this.MoveBetweenPositions(new Vector2(-500.0f, 0.0f), new Vector2(0.0f, 0.0f), 0.5f, () => {
 				this.EndShow();
 			}));
-			
+
 			base.Show();
 		}
-		
+
 		public override void Dismiss() {
 			this.StartCoroutine(this.MoveBetweenPositions(new Vector2(0.0f, 0.0f), new Vector2(500.0f, 0.0f), 0.5f, () => {
 				this.EndDismiss();
+        Toolbox.GetInstance<ObjectPoolManager>().Recycle(this.gameObject);
 			}));
-			
+
 			base.Dismiss();
 		}
-		
-		
+
+
 		// PRAGMA MARK - Button Callbacks
 		public void OnStartGameTapped() {
 			this._viewController.OnStartGameTapped();
 		}
-		
-		
+
+
 		// PRAGMA MARK - Internal
 		[SerializeField] private Text _title;
 		[SerializeField] private Text _startGameButtonText;
-		
+
 		protected IEnumerator MoveBetweenPositions(Vector2 startPosition, Vector2 endPosition, float time, Action callback) {
 			this.transform.localPosition = startPosition;
-			
+
 			for (float elapsedTime = 0;; elapsedTime += Time.deltaTime) {
 				this.transform.localPosition = Vector2.Lerp(startPosition, endPosition, Easers.Ease(EaseType.QuadOut, 0.0f, 1.0f, elapsedTime, time));
-				
+
 				if (elapsedTime >= time) {
 					break;
 				}
 				yield return new WaitForEndOfFrame();
 			}
-			
+
 			callback();
 		}
 	}
